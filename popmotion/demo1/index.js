2b282ac8b2c08cf8584dd15f143719e514b2ee53
HC <huangchun_cd@keruyun.com>
2018-01-30 23:44:23 +0800

    add popmotion

---
+++ popmotion/demo1/index.js
@@ -0,0 +1,16 @@
+import {  styler, spring, listen, pointer, value } from 'popmotion';
+
+const ball = document.querySelector('.box');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    ballXY.stop();
+  });
