eac98cbbaeaa9e65a2685abd5e2e352b3c4c3a3f
HC <huangchun_cd@keruyun.com>
2018-08-07 17:00:27 +0800

    bisheng demo

---
+++ bisheng/demo1/_site/posts/good-bye.md.js
@@ -0,0 +1,17 @@
+webpackJsonp([1],{
+
+/***/ 501:
+/***/ (function(module, exports) {
+
+module.exports = {
+  "content": [
+    "article"
+  ],
+  "meta": {
+    "filename": "posts/good-bye.md"
+  }
+};
+
+/***/ })
+
+});
---
+++ bisheng/demo1/bisheng.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  source: './posts',
+  output: './_site',
+  theme: 'bisheng-theme-one',
+  themeConfig: {
+    home: '/',
+    siteName: 'One',
+    tagline: 'The one theme for bisheng',
+  },
+  port: 8000,
+  root: '/bisheng/',
+};
